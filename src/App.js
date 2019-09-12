8b6650dfd8aec7e81649ea3d6d4f42b03d009da1
Luis Arevalo <arevaloluis89@yahoo.com>
2019-09-12 14:22:44 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HomeComponent from "./components/HomeComponent";
-
+import HomeComponent from "./components/HomeComponent/HomeComponent";
+import ServicesComponent from "./containers/ServicesComponent/ServicesComponent";
 function App() {
   return (
     <div className="App">
       <HomeComponent />
-      
+      <ServicesComponent />
     </div>
   );
 }
