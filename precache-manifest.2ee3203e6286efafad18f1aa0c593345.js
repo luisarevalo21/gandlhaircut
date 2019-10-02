562b4f35497697c0c1731a12c319d7d6874ab03b
Jessie Sidhu <jsidhu22@gmail.com>
2019-10-01 17:46:10 -0700

    Updates

---
+++ precache-manifest.2ee3203e6286efafad18f1aa0c593345.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97ade9402c46c1cf58a7104ef95dda7",
+    "url": "/gandlhaircut/index.html"
+  },
+  {
+    "revision": "865834dc3402d72f1ef9",
+    "url": "/gandlhaircut/static/css/2.a65581f3.chunk.css"
+  },
+  {
+    "revision": "beb51d7fe6bf4c152a35",
+    "url": "/gandlhaircut/static/css/main.36df3af9.chunk.css"
+  },
+  {
+    "revision": "865834dc3402d72f1ef9",
+    "url": "/gandlhaircut/static/js/2.2c24b398.chunk.js"
+  },
+  {
+    "revision": "beb51d7fe6bf4c152a35",
+    "url": "/gandlhaircut/static/js/main.eaa321c1.chunk.js"
+  },
+  {
+    "revision": "9b1aa649afa1820f35dc",
+    "url": "/gandlhaircut/static/js/runtime~main.b5e762c9.js"
+  },
+  {
+    "revision": "7955557ffac748b30cf38ece2acc562b",
+    "url": "/gandlhaircut/static/media/AboutImage1Cropped.7955557f.jpg"
+  },
+  {
+    "revision": "7dcaa00e24110d3911dfffefd4b86f26",
+    "url": "/gandlhaircut/static/media/AboutImage2Cropped.7dcaa00e.jpg"
+  },
+  {
+    "revision": "d77e4b9a0477a335884a43361a5200fc",
+    "url": "/gandlhaircut/static/media/AboutImage3Cropped.d77e4b9a.jpg"
+  },
+  {
+    "revision": "d87ca76cae7468e1105612c343d86cf5",
+    "url": "/gandlhaircut/static/media/Glenda2.d87ca76c.jpg"
+  },
+  {
+    "revision": "1c80e03f39a39817ac08f56c34ea823a",
+    "url": "/gandlhaircut/static/media/IMG_0605.1c80e03f.JPG"
+  },
+  {
+    "revision": "a96ab61e69597e397e7d88a6d3afd79b",
+    "url": "/gandlhaircut/static/media/IMG_0606.a96ab61e.JPG"
+  },
+  {
+    "revision": "991620fe5e15ac79fd25563e01edbf62",
+    "url": "/gandlhaircut/static/media/IMG_0619.991620fe.JPG"
+  },
+  {
+    "revision": "a7ce15a8a0304ffb7a07baacf78aeefe",
+    "url": "/gandlhaircut/static/media/IMG_0619.a7ce15a8.JPG"
+  },
+  {
+    "revision": "88286d243bb1ab8baac1eddfe3858a6a",
+    "url": "/gandlhaircut/static/media/Inside1.88286d24.jpg"
+  },
+  {
+    "revision": "8a33e766060c0c75f9711a44bcd44138",
+    "url": "/gandlhaircut/static/media/Inside2.8a33e766.jpg"
+  },
+  {
+    "revision": "0529846ee6bf57c0131052aae47d93be",
+    "url": "/gandlhaircut/static/media/Inside3.0529846e.JPG"
+  },
+  {
+    "revision": "59bb8268dcf8f8a100f771c707097a0c",
+    "url": "/gandlhaircut/static/media/LogoGL.59bb8268.png"
+  },
+  {
+    "revision": "87fb84b179ce2b7303537387f1255f05",
+    "url": "/gandlhaircut/static/media/Lorena.87fb84b1.jpg"
+  },
+  {
+    "revision": "3b2883e7fa30bef16387c71052b4b9a2",
+    "url": "/gandlhaircut/static/media/home1.3b2883e7.jpg"
+  }
+]);
